3cc510f3698d9e82939ec2d4cc7ecdb88f27944f
tortoisetea <athomlinson@my.bcit.ca>
2022-05-30 23:31:41 -0700

    module numbers can change; no longer relies on module number => noises will now only play after first connect/disconnect ===> that's okay though => noises now properly change back if the plugin is deactivated

--- pestercord-noises.plugin.js
+++ pestercord-noises.plugin.js
@@ -10,18 +10,29 @@
  */
 
 module.exports = class PestercordNoises {
-    attemptUpdateSounds() {
-        if(Object.keys(this.modules).length === this.moduleCount) return;
+    updateConnect() {
+        if(this.connectModule) return;
 
         try {
             this.connectModule = Object.values(this.modules).filter(module => module.exports === "/assets/5dd43c946894005258d85770f0d10cff.mp3")[0];
             this.connectModule.exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
         } catch {}
+    }
+
+    updateDisconnect() {
+        if(this.disconnectModule) return;
 
         try {
             this.disconnectModule = Object.values(this.modules).filter(module => module.exports === "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3")[0];
             this.disconnectModule.exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
         } catch {}
+    }
+
+    attemptUpdateSounds() {
+        if((this.connectModule && this.disconnectModule) || Object.keys(this.modules).length === this.moduleCount) return;
+
+        this.updateConnect();
+        this.updateDisconnect();
     }
 
     load() {
@@ -35,11 +46,24 @@ module.exports = class PestercordNoises {
         this.moduleCount = Object.keys(this.modules).length;
     }
 
-    start() { }
+    start() {
+        if(this.connectModule) {
+            this.connectModule.exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
+        } else this.updateConnect();
+
+        if(this.disconnectModule) {
+            this.disconnectModule.exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
+        } else this.updateDisconnect();
+    }
 
     stop() {
-        try { this.connectModule.exports = "/assets/5dd43c946894005258d85770f0d10cff.mp3"; } catch { }
-        try { this.disconnectModule.exports = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3"; } catch { }
+        try {
+            this.connectModule.exports = "/assets/5dd43c946894005258d85770f0d10cff.mp3";
+        } catch {}
+
+        try {
+            this.disconnectModule.exports = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3";
+        } catch {}
     }
 
     observer(_) {
