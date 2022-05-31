abf0a87116422254dc175297db1de470ddb2512c
tortoisetea <athomlinson@my.bcit.ca>
2022-05-31 13:55:21 -0700

    noise changes! now featuring: chat noises & other people disconnecting noise => chat noise is now the pesterchum chat start noise => join noise is now a edited downpitched pesterchum chat start noise (to make it distinct from the chat noise.... i am not good at audio editing) => the noise when other people disconnect from a call is now also the disconnect noise (i forgot there was a different one when you leave vs when others leave and i was only testing on my own)

--- pestercord-noises.plugin.js
+++ pestercord-noises.plugin.js
@@ -9,13 +9,22 @@
  * @source https://github.com/luminaryFlowers/pestercord-noises
  */
 
+const CONNECT_LINK = "https://lumens.live/resources/pestercordnoises/join.mp3";
+const DISCONNECT_LINK = "https://lumens.live/resources/pestercordnoises/cease.mp3";
+const CHAT_LINK = "https://lumens.live/resources/pestercordnoises/alarm.mp3";
+
+const DEF_CONNECT = "/assets/5dd43c946894005258d85770f0d10cff.mp3";
+const DEF_DISCONNECT = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3";
+const DEF_OTHERDISCONNECT = "/assets/4fcfeb2cba26459c4750e60f626cebdc.mp3"
+const DEF_CHAT = "/assets/dd920c06a01e5bb8b09678581e29d56f.mp3";
+
 module.exports = class PestercordNoises {
     updateConnect() {
         if(this.connectModule) return;
 
         try {
-            this.connectModule = Object.values(this.modules).filter(module => module.exports === "/assets/5dd43c946894005258d85770f0d10cff.mp3")[0];
-            this.connectModule.exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
+            this.connectModule = Object.values(this.modules).filter(module => module.exports === DEF_CONNECT)[0];
+            this.connectModule.exports = CONNECT_LINK;
         } catch {}
     }
 
@@ -23,16 +32,36 @@ module.exports = class PestercordNoises {
         if(this.disconnectModule) return;
 
         try {
-            this.disconnectModule = Object.values(this.modules).filter(module => module.exports === "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3")[0];
-            this.disconnectModule.exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
+            this.disconnectModule = Object.values(this.modules).filter(module => module.exports === DEF_DISCONNECT)[0];
+            this.disconnectModule.exports = DISCONNECT_LINK;
+        } catch {}
+    }
+
+    updateOtherDisconnect() {
+        if(this.otherDisconnectModule) return;
+
+        try {
+            this.otherDisconnectModule = Object.values(this.modules).filter(module => module.exports === DEF_OTHERDISCONNECT)[0];
+            this.otherDisconnectModule.exports = DISCONNECT_LINK;
+        } catch {}
+    }
+
+    updateChatNoise() {
+        if(this.chatModule) return;
+
+        try {
+            this.chatModule = Object.values(this.modules).filter(module => module.exports === DEF_CHAT)[0];
+            this.chatModule.exports = CHAT_LINK;
         } catch {}
     }
 
     attemptUpdateSounds() {
-        if((this.connectModule && this.disconnectModule) || Object.keys(this.modules).length === this.moduleCount) return;
+        if(Object.keys(this.modules).length === this.moduleCount) return;
 
         this.updateConnect();
         this.updateDisconnect();
+        this.updateOtherDisconnect();
+        this.updateChatNoise();
     }
 
     load() {
@@ -48,21 +77,37 @@ module.exports = class PestercordNoises {
 
     start() {
         if(this.connectModule) {
-            this.connectModule.exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
+            this.connectModule.exports = CONNECT_LINK;
         } else this.updateConnect();
 
         if(this.disconnectModule) {
-            this.disconnectModule.exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
+            this.disconnectModule.exports = DISCONNECT_LINK;
         } else this.updateDisconnect();
+
+        if(this.otherDisconnectModule) {
+            this.otherDisconnectModule.exports = DISCONNECT_LINK;
+        } else this.updateOtherDisconnect();
+
+        if(this.chatModule) {
+            this.chatModule.exports = CHAT_LINK;
+        } else this.updateChatNoise();
     }
 
     stop() {
         try {
-            this.connectModule.exports = "/assets/5dd43c946894005258d85770f0d10cff.mp3";
+            this.connectModule.exports = DEF_CONNECT;
         } catch {}
 
         try {
-            this.disconnectModule.exports = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3";
+            this.disconnectModule.exports = DEF_DISCONNECT;
+        } catch {}
+
+        try {
+            this.otherDisconnectModule.exports = DEF_OTHERDISCONNECT;
+        } catch {}
+
+        try {
+            this.chatModule.exports = DEF_CHAT;
         } catch {}
     }
 
