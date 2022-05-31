e81e1f2ae8b9092211c6fbcbffb2228cf5f0afc5
tortoisetea <athomlinson@my.bcit.ca>
2022-05-30 22:33:34 -0700

    module numbers can change; no longer relies on module number => noises will now only play after first connect/disconnect => that's okay though

--- pestercord-noises.plugin.js
+++ pestercord-noises.plugin.js
@@ -9,14 +9,21 @@
  * @source https://github.com/luminaryFlowers/pestercord-noises
  */
 
-/*
-CONNECT: 835579
-DISCONNECT: 279686
-
-CHAT RECEIVE: 423796
-*/
-
 module.exports = class PestercordNoises {
+    attemptUpdateSounds() {
+        if(Object.keys(this.modules).length === this.moduleCount) return;
+
+        try {
+            this.connectModule = Object.values(this.modules).filter(module => module.exports === "/assets/5dd43c946894005258d85770f0d10cff.mp3")[0];
+            this.connectModule.exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
+        } catch {}
+
+        try {
+            this.disconnectModule = Object.values(this.modules).filter(module => module.exports === "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3")[0];
+            this.disconnectModule.exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
+        } catch {}
+    }
+
     load() {
         let id = "bd-webpackmodules";
         let __discord_webpack_require__;
@@ -25,15 +32,17 @@ module.exports = class PestercordNoises {
         }, [[id]]]);
 
         this.modules = __discord_webpack_require__.c;
+        this.moduleCount = Object.keys(this.modules).length;
     }
 
-    start() {
-        this.modules[835579].exports = "https://64.media.tumblr.com/13f42345f60d588f7558598a811ef2ff/73268037f138ba07-fb/5609f0f3eadb7f264df33dd33d1a0e81617967b8.mp3";
-        this.modules[279686].exports = "https://64.media.tumblr.com/64a1041c8cc41b988a3fe5dc9f381b1b/73268037f138ba07-49/a4fb855559f052b40d25495acbb8a811fcf6fd12.mp3";
-    }
+    start() { }
 
     stop() {
-        // this.modules[835579].exports = "/assets/5dd43c946894005258d85770f0d10cff.mp3";
-        // this.modules[279686].exports = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3";
+        try { this.connectModule.exports = "/assets/5dd43c946894005258d85770f0d10cff.mp3"; } catch { }
+        try { this.disconnectModule.exports = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3"; } catch { }
+    }
+
+    observer(_) {
+        this.attemptUpdateSounds();
     }
 }
