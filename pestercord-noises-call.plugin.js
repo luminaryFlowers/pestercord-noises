cfb5c2bbe45d94ca780cdb24b689e379403bb161
tortoisetea <athomlinson@my.bcit.ca>
2022-05-31 14:11:34 -0700

    changed description for default, and added a version that only changes call connect/disconnect noises

--- pestercord-noises-call.plugin.js
+++ pestercord-noises-call.plugin.js
@@ -1,7 +1,7 @@
 /**
- * @name PestercordNoises
+ * @name PestercordNoisesCall
  * @author luminaryFlowers
- * @description This plugin changes some Discord noises to be Pesterchum themed.
+ * @description This plugin changes the default Discord call connect/disconnect sounds to be the Pesterchum chat open/close noises.
  * @version 1.1.0
  * @authorLink https://lumenscode.tumblr.com/
  * @donate https://ko-fi.com/lumens
@@ -9,16 +9,14 @@
  * @source https://github.com/luminaryFlowers/pestercord-noises
  */
 
-const CONNECT_LINK = "https://lumens.live/resources/pestercordnoises/join.mp3";
+const CONNECT_LINK = "https://lumens.live/resources/pestercordnoises/alarm.mp3";
 const DISCONNECT_LINK = "https://lumens.live/resources/pestercordnoises/cease.mp3";
-const CHAT_LINK = "https://lumens.live/resources/pestercordnoises/alarm.mp3";
 
 const DEF_CONNECT = "/assets/5dd43c946894005258d85770f0d10cff.mp3";
 const DEF_DISCONNECT = "/assets/7e125dc075ec6e5ae796e4c3ab83abb3.mp3";
 const DEF_OTHERDISCONNECT = "/assets/4fcfeb2cba26459c4750e60f626cebdc.mp3"
-const DEF_CHAT = "/assets/dd920c06a01e5bb8b09678581e29d56f.mp3";
 
-module.exports = class PestercordNoises {
+module.exports = class PestercordNoisesCall {
     updateConnect() {
         if(this.connectModule) return;
 
@@ -46,22 +44,12 @@ module.exports = class PestercordNoises {
         } catch {}
     }
 
-    updateChatNoise() {
-        if(this.chatModule) return;
-
-        try {
-            this.chatModule = Object.values(this.modules).filter(module => module.exports === DEF_CHAT)[0];
-            this.chatModule.exports = CHAT_LINK;
-        } catch {}
-    }
-
     attemptUpdateSounds() {
         if(Object.keys(this.modules).length === this.moduleCount) return;
 
         this.updateConnect();
         this.updateDisconnect();
         this.updateOtherDisconnect();
-        this.updateChatNoise();
     }
 
     load() {
@@ -87,10 +75,6 @@ module.exports = class PestercordNoises {
         if(this.otherDisconnectModule) {
             this.otherDisconnectModule.exports = DISCONNECT_LINK;
         } else this.updateOtherDisconnect();
-
-        if(this.chatModule) {
-            this.chatModule.exports = CHAT_LINK;
-        } else this.updateChatNoise();
     }
 
     stop() {
@@ -104,10 +88,6 @@ module.exports = class PestercordNoises {
 
         try {
             this.otherDisconnectModule.exports = DEF_OTHERDISCONNECT;
-        } catch {}
-
-        try {
-            this.chatModule.exports = DEF_CHAT;
         } catch {}
     }
 
